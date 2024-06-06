845d79f1cbfb26d989dfe72c37291f9c469fd84f
lollipop <liujian594@hotmail.com>
2024-06-06 22:06:29 +0800

    feat: 先创建一个服务，壁纸的API还要去温习一下

---
+++ app/src/main/java/com/lollipop/photo/wallpaper/PhotoWallpaperService.kt
@@ -0,0 +1,10 @@
+package com.lollipop.photo.wallpaper
+
+import android.service.wallpaper.WallpaperService
+
+class PhotoWallpaperService: WallpaperService() {
+    override fun onCreateEngine(): Engine {
+        TODO("Not yet implemented")
+    }
+
+}
